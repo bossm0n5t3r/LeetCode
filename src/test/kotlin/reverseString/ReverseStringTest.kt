83bcb011a577e5199566df9435fd3bb51cf8ab1c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-04 01:41:21 +0900

    344. Reverse String

--- src/test/kotlin/reverseString/ReverseStringTest.kt
+++ src/test/kotlin/reverseString/ReverseStringTest.kt
@@ -6,20 +6,38 @@ import org.junit.jupiter.api.Test
 class ReverseStringTest {
     private val solution = ReverseString()
 
+    data class ReverseStringTestData(
+        val s: CharArray,
+        val result: CharArray
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as ReverseStringTestData
+
+            if (!s.contentEquals(other.s)) return false
+            if (!result.contentEquals(other.result)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = s.contentHashCode()
+            result = 31 * result + this.result.contentHashCode()
+            return result
+        }
+    }
+
     @Test
     fun reverseString() {
-        val input = listOf(
-            "hello".toCharArray(),
-            "Hannah".toCharArray(),
-            "A man, a plan, a canal: Panama".toCharArray()
+        val tests = listOf(
+            ReverseStringTestData("hello".toCharArray(), "olleh".toCharArray()),
+            ReverseStringTestData("Hannah".toCharArray(), "hannaH".toCharArray()),
+            ReverseStringTestData("A man, a plan, a canal: Panama".toCharArray(), "amanaP :lanac a ,nalp a ,nam A".toCharArray()),
         )
-        val result = listOf(
-            "olleh".toCharArray(),
-            "hannaH".toCharArray(),
-            "amanaP :lanac a ,nalp a ,nam A".toCharArray()
-        )
-        input.forEachIndexed { index, s ->
-            assertTrue(solution.reverseString(s).contentEquals(result[index]))
+        tests.forEach { test ->
+            assertTrue(solution.reverseString(test.s).contentEquals(test.result))
         }
     }
 }
