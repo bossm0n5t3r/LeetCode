f3f2ec3556285c7d10445c8f6331bdccd9d5b770
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-05 15:33:49 +0900

    104. Maximum Depth of Binary Tree

--- src/main/kotlin/maximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.kt
+++ src/main/kotlin/maximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.kt
@@ -3,16 +3,6 @@ package maximumDepthOfBinaryTree
 import java.util.LinkedList
 import java.util.Queue
 
-/**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
- * class TreeNode(var `val`: Int) {
- *     var left: TreeNode? = null
- *     var right: TreeNode? = null
- * }
- */
 class MaximumDepthOfBinaryTree {
     fun maxDepth(root: TreeNode?): Int {
         if (root == null) return 0
