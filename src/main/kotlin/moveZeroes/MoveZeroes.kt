61df64a83dc8eaf783afa0212842bb85811bb502
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-07 00:02:30 +0900

    283. Move Zeroes

--- src/main/kotlin/moveZeroes/MoveZeroes.kt
+++ src/main/kotlin/moveZeroes/MoveZeroes.kt
@@ -1,20 +1,15 @@
 package moveZeroes
 
 class MoveZeroes {
-    private var tmp: Int = 0
-
     fun moveZeroes(nums: IntArray) {
         var lastNonZeroIndex = 0
-        for (cur in nums.indices) {
-            if (nums[cur] != 0) {
-                swap(nums, lastNonZeroIndex++, cur)
+        for (i in nums.indices) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroIndex++] = nums[i]
             }
         }
-    }
-
-    private fun swap(nums: IntArray, i: Int, j: Int) {
-        tmp = nums[i]
-        nums[i] = nums[j]
-        nums[j] = tmp
+        for (i in lastNonZeroIndex until nums.size) {
+            nums[i] = 0
+        }
     }
 }
