c277a9e041d732e646e594819c08812e1dd30088
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-10 01:12:30 +0900

    21. Merge Two Sorted Lists

--- src/main/kotlin/mergeTwoSortedLists/MergeTwoSortedLists.kt
+++ src/main/kotlin/mergeTwoSortedLists/MergeTwoSortedLists.kt
@@ -43,15 +43,9 @@ class MergeTwoSortedLists {
     }
 
     fun mergeTwoListsRecursive(l1: ListNode?, l2: ListNode?): ListNode? {
-        if (l1 == null && l2 == null) {
-            return null
-        }
-        if (l1 == null) {
-            return l2
-        }
-        if (l2 == null) {
-            return l1
-        }
+        if (l1 == null && l2 == null) return null
+        if (l1 == null) return l2
+        if (l2 == null) return l1
         return if (l1.`val` < l2.`val`) {
             l1.next = mergeTwoListsRecursive(l1.next, l2)
             l1
