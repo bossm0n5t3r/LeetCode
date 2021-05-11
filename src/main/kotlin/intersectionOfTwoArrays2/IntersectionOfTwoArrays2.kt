b41f8d48baf0aa1a1a26d4a8a0a3e373e771c77d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-11 20:53:20 +0900

    350. Intersection of Two Arrays II

---
+++ src/main/kotlin/intersectionOfTwoArrays2/IntersectionOfTwoArrays2.kt
@@ -0,0 +1,17 @@
+package intersectionOfTwoArrays2
+
+class IntersectionOfTwoArrays2 {
+    fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+        val result = mutableListOf<Int>()
+        val map1 = mutableMapOf<Int, Int>()
+        nums1.forEach { map1[it] = map1.getOrDefault(it, 0) + 1 }
+        nums2.forEach {
+            val count = map1.getOrDefault(it, 0)
+            if (count > 0) {
+                result.add(it)
+                map1[it] = count - 1
+            }
+        }
+        return result.toIntArray()
+    }
+}
