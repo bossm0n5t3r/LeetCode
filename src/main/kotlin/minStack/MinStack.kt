592d15784588b449270318fff8a7479a65c25c57
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-16 18:13:00 +0900

    155. Min Stack

---
+++ src/main/kotlin/minStack/MinStack.kt
@@ -0,0 +1,30 @@
+package minStack
+
+class MinStack() {
+    /** initialize your data structure here. */
+    private val s = mutableListOf<Int>()
+    private var min = Int.MAX_VALUE
+
+    fun push(`val`: Int) {
+        s.add(`val`)
+        if (`val` < min) {
+            min = `val`
+        }
+    }
+
+    fun pop() {
+        val last = s.last()
+        s.removeAt(s.lastIndex)
+        if (last == min) {
+            min = if (s.isEmpty()) Int.MAX_VALUE else s.minOrNull()!!
+        }
+    }
+
+    fun top(): Int {
+        return s.last()
+    }
+
+    fun getMin(): Int {
+        return min
+    }
+}
