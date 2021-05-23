c187ad905a2704f9081870ede9caa8112f32800a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-23 14:25:09 +0900

    20. Valid Parentheses

---
+++ src/main/kotlin/validParentheses/README.md
@@ -0,0 +1,3 @@
+# 20. Valid Parentheses
+
+- [https://leetcode.com/problems/valid-parentheses/](https://leetcode.com/problems/valid-parentheses/)
---
+++ src/main/kotlin/validParentheses/ValidParentheses.kt
@@ -0,0 +1,21 @@
+package validParentheses
+
+class ValidParentheses {
+    fun isValid(s: String): Boolean {
+        val map = mapOf(
+            ')' to '(',
+            '}' to '{',
+            ']' to '['
+        )
+        val stack = mutableListOf<Char>()
+        s.forEach { c ->
+            if (c in map.keys) {
+                if (stack.isEmpty() || stack.last() != map[c]) return false
+                stack.removeAt(stack.size - 1)
+            } else {
+                stack.add(c)
+            }
+        }
+        return stack.isEmpty()
+    }
+}
