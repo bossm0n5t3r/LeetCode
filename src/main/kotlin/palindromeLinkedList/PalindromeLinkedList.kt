6e8f0ea39e51a7ce82b50890e14bf33cb8fc77f1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-05-23 14:09:39 +0900

    234. Palindrome Linked List

---
+++ src/main/kotlin/palindromeLinkedList/PalindromeLinkedList.kt
@@ -0,0 +1,52 @@
+package palindromeLinkedList
+
+import utils.ListNode
+
+class PalindromeLinkedList {
+    /**
+     * 1.
+     *      1. head ListNode 로 부터 reverseListNode 를 구한다.
+     *      2. 처음부터 끝까지 비교한다.
+     *      ㄴ time complexity : O(2n)
+     *      ㄴ space complexity : O(n)
+     * 2. (current code)
+     *      1. 중간 ListNode 를 구한다.
+     *      2. 그리고 중간 ListNode 부터 reverseListNode 를 구한다.
+     *      3. 절반만 비교한다.
+     *      ㄴ time complexity : O(n)
+     *      ㄴ space complexity : O(n/2)
+     */
+    fun isPalindrome(head: ListNode?): Boolean {
+        val midListNode = getMidListNode(head)
+        var reverseHalfListNode = getReverseListNode(midListNode)
+        var cur = head
+        while (reverseHalfListNode != null && cur != null) {
+            if (reverseHalfListNode.`val` != cur.`val`) return false
+            reverseHalfListNode = reverseHalfListNode.next
+            cur = cur.next
+        }
+        return reverseHalfListNode == null || cur == null
+    }
+
+    private fun getMidListNode(head: ListNode?): ListNode? {
+        var half = head
+        var all = head
+        while (all != null) {
+            half = half?.next
+            all = all.next?.next
+        }
+        return half
+    }
+
+    private fun getReverseListNode(head: ListNode?): ListNode? {
+        var cur = head
+        var reverseListNode: ListNode? = null
+        while (cur != null) {
+            val upper = ListNode(cur.`val`)
+            upper.next = reverseListNode
+            reverseListNode = upper
+            cur = cur.next
+        }
+        return reverseListNode
+    }
+}
---
+++ src/test/kotlin/palindromeLinkedList/PalindromeLinkedListTest.kt
@@ -0,0 +1,37 @@
+package palindromeLinkedList
+
+import org.junit.jupiter.api.Test
+import utils.ListNode
+import utils.ListNodeUtil
+import kotlin.test.assertEquals
+
+class PalindromeLinkedListTest {
+    private val palindromeLinkedList = PalindromeLinkedList()
+    private val listNodeUtil = ListNodeUtil()
+
+    data class PalindromeLinkedListTestData(
+        val head: ListNode?,
+        val result: Boolean
+    )
+
+    @Test
+    fun isPalindrome() {
+        val tests = listOf(
+            PalindromeLinkedListTestData(
+                listNodeUtil.generateListNodeFromList(listOf(1, 2, 2, 1)),
+                true
+            ),
+            PalindromeLinkedListTestData(
+                listNodeUtil.generateListNodeFromList(listOf(1, 2, 1, 2, 1)),
+                true
+            ),
+            PalindromeLinkedListTestData(
+                listNodeUtil.generateListNodeFromList(listOf(1, 2)),
+                false
+            )
+        )
+        tests.forEach { test ->
+            assertEquals(palindromeLinkedList.isPalindrome(test.head), test.result)
+        }
+    }
+}
