b5485005a0364028e57a52052e66c2ff25d76105
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-06-02 01:50:19 +0900

    69. Sqrt(x)

---
+++ src/test/kotlin/sqrtX/SqrtXTest.kt
@@ -0,0 +1,26 @@
+package sqrtX
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SqrtXTest {
+    private val sqrtX = SqrtX()
+
+    data class SqrtXTestData(
+        val x: Int,
+        val result: Int
+    )
+
+    @Test
+    fun mySqrt() {
+        val tests = listOf(
+            SqrtXTestData(4, 2),
+            SqrtXTestData(8, 2),
+            SqrtXTestData(2147395600, 46340)
+        )
+        tests.forEach { test ->
+            assertEquals(sqrtX.mySqrtFirst(test.x), test.result)
+            assertEquals(sqrtX.mySqrtSecond(test.x), test.result)
+        }
+    }
+}
