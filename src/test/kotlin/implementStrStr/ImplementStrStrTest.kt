943e2cd669df5263b0c04762b9eafe2a0f1fd598
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-06-08 03:37:00 +0900

    28. Implement strStr()

---
+++ src/test/kotlin/implementStrStr/ImplementStrStrTest.kt
@@ -0,0 +1,28 @@
+package implementStrStr
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ImplementStrStrTest {
+    private val implementStrStr = ImplementStrStr()
+
+    data class ImplementStrStrTestData(
+        val haystack: String,
+        val needle: String,
+        val result: Int
+    )
+
+    @Test
+    fun strStr() {
+        val tests = listOf(
+            ImplementStrStrTestData("hello", "ll", 2),
+            ImplementStrStrTestData("aaaaa", "bba", -1),
+            ImplementStrStrTestData("", "", 0),
+            ImplementStrStrTestData("a", "a", 0),
+            ImplementStrStrTestData("abc", "c", 2)
+        )
+        tests.forEach { test ->
+            assertEquals(implementStrStr.strStr(test.haystack, test.needle), test.result)
+        }
+    }
+}
