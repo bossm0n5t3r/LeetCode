53c09100b6355663639c8e089c2fcd48d9e47adb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-06-25 21:00:00 +0900

    78. Subsets

---
+++ src/main/kotlin/subsets/README.md
@@ -0,0 +1,3 @@
+# 78. Subsets
+
+- [https://leetcode.com/problems/subsets/](https://leetcode.com/problems/subsets/)
---
+++ src/main/kotlin/subsets/Subsets.kt
@@ -0,0 +1,20 @@
+package subsets
+
+class Subsets {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+        val target = mutableListOf<Int>()
+        dfs(result, target, nums, 0)
+        return result
+    }
+
+    private fun dfs(result: MutableList<List<Int>>, target: MutableList<Int>, nums: IntArray, index: Int) {
+        if (index == nums.size) {
+            result.add(target)
+            return
+        }
+        dfs(result, target.toMutableList(), nums, index + 1)
+        target.add(nums[index])
+        dfs(result, target.toMutableList(), nums, index + 1)
+    }
+}
