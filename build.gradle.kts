b7ec5eb76756ac785ad971265da11e0f9310a901
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-07-17 18:07:00 +0900

    371. Sum of Two Integers

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,11 @@ apply(plugin = "org.jlleitschuh.gradle.ktlint")
 group = "me.bossm0n5t3r"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.assertj:assertj-core:3.19.0")
 }
 
 tasks.test {
---
+++ src/main/kotlin/sumOfTwoIntegers/README.md
@@ -0,0 +1,3 @@
+# 371. Sum of Two Integers
+
+- [https://leetcode.com/problems/sum-of-two-integers/](https://leetcode.com/problems/sum-of-two-integers/)
