db19e03f2f8dde61240d587dab29382a522f934b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2021-07-31 19:43:55 +0900

    feat, test: 기본 구조 및 테스트 코드 추가

---
+++ src/main/kotlin/kthSmallestElementInABST/KthSmallestElementInABST.kt
@@ -0,0 +1,9 @@
+package kthSmallestElementInABST
+
+import utils.TreeNode
+
+class KthSmallestElementInABST {
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        return -1
+    }
+}
---
+++ src/test/kotlin/kthSmallestElementInABST/KthSmallestElementInABSTTest.kt
@@ -0,0 +1,31 @@
+package kthSmallestElementInABST
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import utils.TreeNode
+import utils.TreeNodeUtil
+import kotlin.test.assertEquals
+
+class KthSmallestElementInABSTTest {
+    private val kthSmallestElementInABST = KthSmallestElementInABST()
+    private val treeNodeUtil = TreeNodeUtil()
+
+    data class KthSmallestElementInABSTTestData(
+        val root: TreeNode?,
+        val k: Int,
+        val result: Int
+    )
+
+    @Disabled
+    @Test
+    fun kthSmallest() {
+        val tests = listOf(
+            KthSmallestElementInABSTTestData(treeNodeUtil.generateTreeNodeOrNull(listOf(3, 1, 4, null, 2)), 1, 1),
+            KthSmallestElementInABSTTestData(treeNodeUtil.generateTreeNodeOrNull(listOf(5, 3, 6, 2, 4, null, null, 1)), 3, 3)
+        )
+
+        tests.forEach { test ->
+            assertEquals(kthSmallestElementInABST.kthSmallest(test.root, test.k), test.result)
+        }
+    }
+}
