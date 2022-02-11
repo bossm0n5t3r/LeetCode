7a9a35a4d1368b30fc2789bd5b685919ded90939
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-11 21:29:39 +0900

    feat: Update build.gradle.kts
    - Upgrade kotlin version
    - Upgrade ktlint version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.32"
-}
-
-buildscript {
-    val ktlintGradleVersion = "10.0.0"
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:$ktlintGradleVersion")
-    }
+    kotlin("jvm") version "1.6.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 repositories {
     // Required to download KtLint
     mavenCentral()
 }
-
-apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
 group = "me.bossm0n5t3r"
 version = "1.0-SNAPSHOT"
