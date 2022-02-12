d78126958ec81e9948dc301c37dace7622567210
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-13 02:17:30 +0900

    49. Group Anagrams

--- src/main/kotlin/groupAnagrams/GroupAnagrams.kt
+++ src/main/kotlin/groupAnagrams/GroupAnagrams.kt
@@ -2,14 +2,6 @@ package groupAnagrams
 
 class GroupAnagrams {
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        return strs.groupBy { makeKey(it) }.values.toList()
-    }
-
-    private fun makeKey(str: String): String {
-        var result = ""
-        str.toSortedSet().associateWith { str.count { c -> c == it } }.forEach { (char, count) ->
-            result += "$char$count"
-        }
-        return result
+        return strs.groupBy { it.toCharArray().sorted() }.values.toList()
     }
 }
