1144ac49115381c10e1eabf8262fcb32fe8f2b73
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-12 22:35:09 +0900

    289. Game of Life

--- src/main/kotlin/gameOfLife/GameOfLife.kt
+++ src/main/kotlin/gameOfLife/GameOfLife.kt
@@ -4,34 +4,21 @@ class GameOfLife {
     fun gameOfLife(board: Array<IntArray>) {
         val m = board.size
         val n = board.first().size
-        val becomeDie = mutableListOf<Pair<Int, Int>>()
-        val becomeLive = mutableListOf<Pair<Int, Int>>()
         (0 until m).forEach { r ->
             (0 until n).forEach { c ->
-                val curPoint = Pair(r, c)
-                val curCell = board[r][c]
                 val liveNeighborCells = checkLiveNeighbors(board, m, n, r, c)
-                if (curCell == 1) {
-                    when {
-                        // Any live cell with fewer than two live neighbors dies as if caused by under-population.
-                        liveNeighborCells < 2 -> becomeDie.add(curPoint)
-                        // Any live cell with two or three live neighbors lives on to the next generation.
-                        liveNeighborCells in 2..3 -> becomeLive.add(curPoint)
-                        // Any live cell with more than three live neighbors dies, as if by over-population.
-                        liveNeighborCells > 3 -> becomeDie.add(curPoint)
-                    }
+                board[r][c] += liveNeighborCells * 10
+            }
+        }
+        (0 until m).forEach { r ->
+            (0 until n).forEach { c ->
+                if (board[r][c] in setOf(21, 31, 30)) {
+                    board[r][c] = 1
                 } else {
-                    // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-                    if (liveNeighborCells == 3) {
-                        becomeLive.add(curPoint)
-                    } else {
-                        becomeDie.add(curPoint)
-                    }
+                    board[r][c] = 0
                 }
             }
         }
-        becomeLive.forEach { (r, c) -> board[r][c] = 1 }
-        becomeDie.forEach { (r, c) -> board[r][c] = 0 }
     }
 
     private fun checkLiveNeighbors(board: Array<IntArray>, m: Int, n: Int, r: Int, c: Int): Int {
@@ -41,7 +28,7 @@ class GameOfLife {
         (0..7).forEach { i ->
             val nr = r + dr[i]
             val nc = c + dc[i]
-            if ((nr in 0 until m) && (nc in 0 until n) && board[nr][nc] == 1) {
+            if ((nr in 0 until m) && (nc in 0 until n) && board[nr][nc] % 10 == 1) {
                 liveCells++
             }
         }
