a8886816bb182bd180a165afb2416b48d5f066e9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-16 21:15:43 +0900

    328. Odd Even Linked List

--- src/main/kotlin/oddEvenLinkedList/OddEvenLinkedList.kt
+++ src/main/kotlin/oddEvenLinkedList/OddEvenLinkedList.kt
@@ -5,27 +5,16 @@ import utils.ListNode
 class OddEvenLinkedList {
     fun oddEvenList(head: ListNode?): ListNode? {
         if (head == null) return null
-        if (head.next == null || head.next!!.next == null) return head
         var cur = head
-        var tail: ListNode? = null
-        var tailFirst: ListNode? = null
-        while (cur?.next?.next != null) {
-            if (tail == null) {
-                tail = cur.next
-                tailFirst = tail
-            } else {
-                tail.next = tail.next?.next
-                tail = tail.next
-            }
-            cur.next = cur.next!!.next
-            cur = cur.next
-        }
-        if (tail?.next?.next != null) {
+        var tail = head.next
+        val tailHead = tail
+        while (tail?.next != null) {
+            cur?.next = cur?.next!!.next
             tail.next = tail.next?.next
             tail = tail.next
+            cur = cur.next
         }
-        tail?.next = null
-        cur?.next = tailFirst
+        cur?.next = tailHead
         return head
     }
 }
