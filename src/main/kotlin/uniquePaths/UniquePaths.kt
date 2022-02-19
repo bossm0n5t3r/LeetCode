b278ee36303ef778849585fbed660ee415d9fcc3
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-20 00:45:30 +0900

    62. Unique Paths

--- src/main/kotlin/uniquePaths/UniquePaths.kt
+++ src/main/kotlin/uniquePaths/UniquePaths.kt
@@ -1,18 +1,18 @@
 package uniquePaths
 
-import java.math.BigInteger
-import kotlin.math.min
-
 class UniquePaths {
     fun uniquePaths(m: Int, n: Int): Int {
         val min = min(m - 1, n - 1)
         val sum = m + n - 2
-        var numerator = BigInteger("1")
-        var denominator = BigInteger("1")
+        var result = 1L
         for (i in 0 until min) {
-            numerator = numerator.multiply(BigInteger((sum - i).toString()))
-            denominator = denominator.multiply(BigInteger((i + 1).toString()))
+            result *= (sum - i)
+            result /= (i + 1)
         }
-        return (numerator.divide(denominator)).toInt()
+        return result.toInt()
+    }
+
+    private fun min(m: Int, n: Int): Int {
+        return if (m < n) m else n
     }
 }
