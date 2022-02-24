9698e372731abd162021102efc1fb9f6d3bbe7a0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-25 04:10:20 +0900

    230. Kth Smallest Element in a BST

--- src/main/kotlin/kthSmallestElementInABST/KthSmallestElementInABST.kt
+++ src/main/kotlin/kthSmallestElementInABST/KthSmallestElementInABST.kt
@@ -3,25 +3,29 @@ package kthSmallestElementInABST
 import utils.TreeNode
 
 class KthSmallestElementInABST {
-    companion object {
-        var count = 0
-        var number = 0
-    }
-
     fun kthSmallest(root: TreeNode?, k: Int): Int {
         if (root == null) return -1
-        count = k
-        dfs(root)
-        return number
+        val count = IntWrapper()
+        val number = IntWrapper()
+        dfs(root, k, count, number)
+        return number.value
     }
 
-    private fun dfs(cur: TreeNode) {
-        if (cur.left != null) dfs(cur.left!!)
-        count--
-        if (count == 0) {
-            number = cur.`val`
+    data class IntWrapper(
+        var value: Int = 0
+    ) {
+        fun increment() {
+            value++
+        }
+    }
+
+    private fun dfs(cur: TreeNode, k: Int, count: IntWrapper, number: IntWrapper) {
+        if (cur.left != null) dfs(cur.left!!, k, count, number)
+        count.increment()
+        if (count.value == k) {
+            number.value = cur.`val`
             return
         }
-        if (cur.right != null) dfs(cur.right!!)
+        if (cur.right != null) dfs(cur.right!!, k, count, number)
     }
 }
