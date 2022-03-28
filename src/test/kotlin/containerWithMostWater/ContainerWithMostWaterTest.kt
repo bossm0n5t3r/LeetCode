cec582153486418793b9efa6902bd19d7d3b83f2
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-03-29 02:50:11 +0900

    11. Container With Most Water

---
+++ src/test/kotlin/containerWithMostWater/ContainerWithMostWaterTest.kt
@@ -0,0 +1,53 @@
+package containerWithMostWater
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class ContainerWithMostWaterTest {
+    private val containerWithMostWater = ContainerWithMostWater()
+
+    private data class ContainerWithMostWaterTestData(
+        val height: IntArray,
+        val result: Int
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as ContainerWithMostWaterTestData
+
+            if (!height.contentEquals(other.height)) return false
+            if (result != other.result) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result1 = height.contentHashCode()
+            result1 = 31 * result1 + result
+            return result1
+        }
+    }
+
+    @Test
+    fun maxArea() {
+        val tests = listOf(
+            ContainerWithMostWaterTestData(
+                intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7),
+                49
+            ),
+            ContainerWithMostWaterTestData(
+                intArrayOf(1, 1),
+                1
+            ),
+            ContainerWithMostWaterTestData(
+                intArrayOf(1, 2, 1),
+                2
+            ),
+        )
+        tests.forEach { test ->
+            println(containerWithMostWater.maxArea(test.height))
+            assertEquals(containerWithMostWater.maxArea(test.height), test.result)
+        }
+    }
+}
