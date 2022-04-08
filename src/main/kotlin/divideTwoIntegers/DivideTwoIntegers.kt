9149556efffff2f6f66e1292d6f5b54c219dfe86
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-04-09 04:23:20 +0900

    29. Divide Two Integers

---
+++ src/main/kotlin/divideTwoIntegers/DivideTwoIntegers.kt
@@ -0,0 +1,27 @@
+package divideTwoIntegers
+
+class DivideTwoIntegers {
+    fun divide(dividend: Int, divisor: Int): Int {
+        if (dividend == 1 shl 31 && divisor == -1) return Int.MAX_VALUE
+        var result = 0
+        var tmpDividend = abs(dividend)
+        val tmpDivisor = abs(divisor)
+        (31 downTo 0).forEach { i ->
+            if ((tmpDividend shr i) - tmpDivisor >= 0) {
+                result += (1 shl i)
+                tmpDividend -= (tmpDivisor shl i)
+            }
+        }
+        return if (dividend > 0 == divisor > 0) {
+            result
+        } else {
+            0 - result
+        }
+    }
+
+    private fun abs(a: Int) = if (a >= 0) {
+        a.toLong()
+    } else {
+        0L - a.toLong()
+    }
+}
---
+++ src/main/kotlin/divideTwoIntegers/README.md
@@ -0,0 +1,3 @@
+# 29. Divide Two Integers
+
+- [https://leetcode.com/problems/divide-two-integers/](https://leetcode.com/problems/divide-two-integers/)
