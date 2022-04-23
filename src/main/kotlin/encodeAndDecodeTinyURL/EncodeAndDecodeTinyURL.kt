a9c20120084206e8a619703f4e0f7cd22b41ea75
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-04-24 00:49:49 +0900

    535. Encode and Decode TinyURL

--- src/main/kotlin/encodeAndDecodeTinyURL/EncodeAndDecodeTinyURL.kt
+++ src/main/kotlin/encodeAndDecodeTinyURL/EncodeAndDecodeTinyURL.kt
@@ -1,21 +1,38 @@
 package encodeAndDecodeTinyURL
 
+import kotlin.random.Random
+
 class EncodeAndDecodeTinyURL {
-    private val hashCodeToLongUrl = mutableMapOf<String, String>()
+    private val BASE_HOST = "http://tinyurl.com/"
+    private val shortUrlToLongUrl = mutableMapOf<String, String>()
+    private val longUrlToShortUrl = mutableMapOf<String, String>()
+    private val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    private val STRING_LENGTH = 6
 
     // Encodes a URL to a shortened URL.
     fun encode(longUrl: String): String {
-        val hashCode = longUrl.hashCode().toString()
-        hashCodeToLongUrl[hashCode] = longUrl
-        return hashCode
+        if (longUrlToShortUrl.containsKey(longUrl)) return BASE_HOST + longUrlToShortUrl[longUrl]
+        var key = ""
+        do {
+            key = (1..STRING_LENGTH)
+                .map { Random.nextInt(0, charPool.size) }
+                .map(charPool::get)
+                .joinToString("")
+        } while (shortUrlToLongUrl.containsKey(key))
+        shortUrlToLongUrl[key] = longUrl
+        longUrlToShortUrl[longUrl] = key
+        return BASE_HOST + key
     }
 
     // Decodes a shortened URL to its original URL.
     fun decode(shortUrl: String): String {
-        return hashCodeToLongUrl[shortUrl]!!
+        return shortUrlToLongUrl[shortUrl.replace(BASE_HOST, "")]!!
     }
 
-    fun cleanUp() = hashCodeToLongUrl.clear()
+    fun cleanUp() {
+        shortUrlToLongUrl.clear()
+        longUrlToShortUrl.clear()
+    }
 
-    fun numberOfKey() = hashCodeToLongUrl.keys.size
+    fun numberOfKey() = shortUrlToLongUrl.keys.size
 }
