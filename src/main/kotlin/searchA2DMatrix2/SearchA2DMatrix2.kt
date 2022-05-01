bf3d259b48249103dbce1c2e66dbd07050c54ad5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-05-02 02:11:06 +0900

    240. Search a 2D Matrix II

---
+++ src/main/kotlin/searchA2DMatrix2/SearchA2DMatrix2.kt
@@ -0,0 +1,27 @@
+package searchA2DMatrix2
+
+import java.util.LinkedList
+
+class SearchA2DMatrix2 {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        val queue = LinkedList<Pair<Int, Int>>()
+        val m = matrix.size
+        val n = matrix.first().size
+        val visited = Array(m) { BooleanArray(n) { false } }
+        queue.add(Pair(0, 0))
+        visited[0][0] = true
+        while (queue.isNotEmpty()) {
+            val (r, c) = queue.poll()
+            if (matrix[r][c] == target) return true
+            if (r + 1 < m && !visited[r + 1][c] && matrix[r + 1][c] <= target) {
+                visited[r + 1][c] = true
+                queue.add(Pair(r + 1, c))
+            }
+            if (c + 1 < n && !visited[r][c + 1] && matrix[r][c + 1] <= target) {
+                visited[r][c + 1] = true
+                queue.add(Pair(r, c + 1))
+            }
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/searchA2DMatrix2/SearchA2DMatrix2Test.kt
@@ -0,0 +1,74 @@
+package searchA2DMatrix2
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class SearchA2DMatrix2Test {
+    private val searchA2DMatrix2 = SearchA2DMatrix2()
+
+    private data class SearchA2DMatrix2TestData(
+        val matrix: Array<IntArray>,
+        val target: Int,
+        val result: Boolean
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as SearchA2DMatrix2TestData
+
+            if (!matrix.contentDeepEquals(other.matrix)) return false
+            if (target != other.target) return false
+            if (result != other.result) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result1 = matrix.contentDeepHashCode()
+            result1 = 31 * result1 + target
+            result1 = 31 * result1 + result.hashCode()
+            return result1
+        }
+    }
+
+    @Test
+    fun searchMatrix() {
+        val tests = listOf(
+            SearchA2DMatrix2TestData(
+                matrix = arrayOf(
+                    intArrayOf(1, 4, 7, 11, 15),
+                    intArrayOf(2, 5, 8, 12, 19),
+                    intArrayOf(3, 6, 9, 16, 22),
+                    intArrayOf(10, 13, 14, 17, 24),
+                    intArrayOf(18, 21, 23, 26, 30),
+                ),
+                target = 5,
+                result = true
+            ),
+            SearchA2DMatrix2TestData(
+                matrix = arrayOf(
+                    intArrayOf(1, 4, 7, 11, 15),
+                    intArrayOf(2, 5, 8, 12, 19),
+                    intArrayOf(3, 6, 9, 16, 22),
+                    intArrayOf(10, 13, 14, 17, 24),
+                    intArrayOf(18, 21, 23, 26, 30),
+                ),
+                target = 20,
+                result = false
+            ),
+            SearchA2DMatrix2TestData(
+                matrix = arrayOf(
+                    intArrayOf(-1, 3),
+                ),
+                target = 3,
+                result = true
+            ),
+        )
+        tests.forEach { test ->
+            val result = searchA2DMatrix2.searchMatrix(test.matrix, test.target)
+            println(result)
+            assertEquals(result, test.result)
+        }
+    }
+}
