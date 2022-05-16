b1e1f60a48ae6a86cc1fd8de67bc8c4f4a808afd
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-05-17 02:33:03 +0900

    1091. Shortest Path in Binary Matrix

---
+++ src/test/kotlin/shortestPathInBinaryMatrix/ShortestPathInBinaryMatrixTest.kt
@@ -0,0 +1,75 @@
+package shortestPathInBinaryMatrix
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class ShortestPathInBinaryMatrixTest {
+    private val shortestPathInBinaryMatrix = ShortestPathInBinaryMatrix()
+
+    private data class ShortestPathInBinaryMatrixTestData(
+        val grid: Array<IntArray>,
+        val result: Int
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as ShortestPathInBinaryMatrixTestData
+
+            if (!grid.contentDeepEquals(other.grid)) return false
+            if (result != other.result) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result1 = grid.contentDeepHashCode()
+            result1 = 31 * result1 + result
+            return result1
+        }
+    }
+
+    @Test
+    fun shortestPathBinaryMatrix() {
+        val tests = listOf(
+            ShortestPathInBinaryMatrixTestData(
+                arrayOf(
+                    intArrayOf(0, 1),
+                    intArrayOf(1, 0),
+                ),
+                2
+            ),
+            ShortestPathInBinaryMatrixTestData(
+                arrayOf(
+                    intArrayOf(0, 0, 0),
+                    intArrayOf(1, 1, 0),
+                    intArrayOf(1, 1, 0),
+                ),
+                4
+            ),
+            ShortestPathInBinaryMatrixTestData(
+                arrayOf(
+                    intArrayOf(1, 0, 0),
+                    intArrayOf(1, 1, 0),
+                    intArrayOf(1, 1, 0),
+                ),
+                -1
+            ),
+            ShortestPathInBinaryMatrixTestData(
+                arrayOf(
+                    intArrayOf(0, 0, 0),
+                    intArrayOf(1, 0, 0),
+                    intArrayOf(1, 1, 0),
+                ),
+                3
+            ),
+        )
+        tests.forEach { test ->
+            shortestPathInBinaryMatrix.shortestPathBinaryMatrix(test.grid)
+                .also {
+                    println(it)
+                    assertEquals(it, test.result)
+                }
+        }
+    }
+}
