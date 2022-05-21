1013ef300b0da9d21f33869f9efc8dc1097b7a3c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-05-22 01:41:40 +0900

    322. Coin Change

--- src/main/kotlin/coinChange/CoinChange.kt
+++ src/main/kotlin/coinChange/CoinChange.kt
@@ -5,20 +5,16 @@ import kotlin.math.min
 class CoinChange {
     fun coinChange(coins: IntArray, amount: Int): Int {
         if (amount == 0) return 0
-        val dp = IntArray(amount + 1) { -1 }
-        coins.forEach { if (it <= amount) { dp[it] = 1 } }
+        val max = amount + 1
+        val dp = IntArray(amount + 1) { max }
+        dp[0] = 0
         (1..amount).forEach {
-            for (coin in coins) {
-                if (it - coin > 0 && dp[it - coin] > 0) {
-                    dp[it] = if (dp[it] == -1) {
-                        dp[it - coin] + 1
-                    } else {
-                        min(dp[it], dp[it - coin] + 1)
-                    }
+            coins.forEach { coin ->
+                if (it - coin >= 0) {
+                    dp[it] = min(dp[it], dp[it - coin] + 1)
                 }
             }
         }
-        println(dp.toList())
-        return dp[amount]
+        return if (dp[amount] > amount) -1 else dp[amount]
     }
 }
