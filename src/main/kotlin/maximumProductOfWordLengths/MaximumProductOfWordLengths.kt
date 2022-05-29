27e69911f68719884f04c84098b6bb40cd806537
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-05-30 03:29:23 +0900

    318. Maximum Product of Word Lengths

--- src/main/kotlin/maximumProductOfWordLengths/MaximumProductOfWordLengths.kt
+++ src/main/kotlin/maximumProductOfWordLengths/MaximumProductOfWordLengths.kt
@@ -2,19 +2,25 @@ package maximumProductOfWordLengths
 
 class MaximumProductOfWordLengths {
     fun maxProduct(words: Array<String>): Int {
+        val stringBits = getStringBits(words)
         var result = 0
-        var left: String
-        var right: String
         (0 until words.size - 1).forEach { i ->
             (i + 1 until words.size).forEach { j ->
-                if (words[i] != words[j] &&
-                    words[i].toSet().intersect(words[j].toSet()).isEmpty() &&
-                    words[i].length * words[j].length > result
-                ) {
-                    left = words[i]
-                    right = words[j]
-                    result = left.length * right.length
+                if (stringBits[i] and stringBits[j] == 0 && words[i].length * words[j].length > result) {
+                    result = words[i].length * words[j].length
                 }
+            }
+        }
+        return result
+    }
+
+    private fun getStringBits(words: Array<String>): IntArray {
+        val n = words.size
+        val result = IntArray(n) { 0 }
+        words.forEachIndexed { index, s ->
+            s.forEach { c ->
+                result[index] = result[index] or (1 shl (c.code - 'a'.code))
+//                result[index] = result[index] or (1 shl (c.toByte().toInt() - 'a'.toByte().toInt()))
             }
         }
         return result
