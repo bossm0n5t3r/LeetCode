0ab8d2c45afb28553aff150279bc9ffa19d539e9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-06-05 01:09:40 +0900

    219. Contains Duplicate II

--- src/main/kotlin/containsDuplicate2/ContainsDuplicate2.kt
+++ src/main/kotlin/containsDuplicate2/ContainsDuplicate2.kt
@@ -1,19 +1,14 @@
 package containsDuplicate2
 
+import kotlin.math.abs
+
 class ContainsDuplicate2 {
     fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
-        if (k == 0) return false
-        if (nums.size == 1) return false
         val n = nums.size
-        var s = 0
-        var e = 1
-        while (e < n) {
-            while (e - s <= k && e < n) {
-                if (nums[s] == nums[e]) return true
-                e++
-            }
-            s++
-            e = s + 1
+        val recent = mutableMapOf<Int, Int>()
+        (0 until n).forEach { i ->
+            if (recent.contains(nums[i]) && abs(recent[nums[i]]!! - i) <= k) return true
+            recent[nums[i]] = i
         }
         return false
     }
