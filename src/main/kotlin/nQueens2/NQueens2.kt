ba862231e65f1c904f97171b6352beb080210e88
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-06-06 00:39:10 +0900

    52. N-Queens II

---
+++ src/main/kotlin/nQueens2/NQueens2.kt
@@ -0,0 +1,39 @@
+package nQueens2
+
+class NQueens2 {
+    fun totalNQueens(n: Int): Int {
+        val result = Result()
+        val board = Array(n) { CharArray(n) { '.' } }
+        dfs(n, board, 0, result)
+        return result.result
+    }
+
+    private data class Result(var result: Int = 0) {
+        fun add() {
+            this.result++
+        }
+    }
+
+    private fun dfs(n: Int, board: Array<CharArray>, row: Int, result: Result) {
+        if (row == n) {
+            result.add()
+            return
+        }
+        (0 until n).forEach { col ->
+            if (isSafe(n, board, row, col)) {
+                board[row][col] = 'Q'
+                dfs(n, board, row + 1, result)
+                board[row][col] = '.'
+            }
+        }
+    }
+
+    private fun isSafe(n: Int, board: Array<CharArray>, row: Int, col: Int): Boolean {
+        (0 until n).forEach { i ->
+            if (board[i][col] == 'Q') return false
+            if (row >= i && col >= i && board[row - i][col - i] == 'Q') return false
+            if (row >= i && col + i < n && board[row - i][col + i] == 'Q') return false
+        }
+        return true
+    }
+}
