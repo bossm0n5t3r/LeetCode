f2adfc515fa51999caf254f54ca2bf559d6665df
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-06-30 21:50:50 +0900

    462. Minimum Moves to Equal Array Elements II

---
+++ src/main/python/minimumMovesToEqualArrayElements2/MinimumMovesToEqualArrayElements2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minMoves2(self, nums: list[int]) -> int:
+        sorted_nums = sorted(nums)
+        median = sorted_nums[len(sorted_nums) // 2]
+        return sum([abs(num - median) for num in nums])
+
+    def test(self):
+        tests = [
+            [[1, 2, 3], 2],
+            [[1, 10, 2, 9], 16],
+        ]
+        for test in tests:
+            nums, expect_result = test
+            result = self.minMoves2(nums)
+            print(result)
+            print(result == expect_result)
+
+
+if __name__ == '__main__':
+    Solution().test()
