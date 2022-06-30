077e66e8d1395d8170ead63789a991fa3b5148ab
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-06-30 21:59:20 +0900

    9. Palindrome Number

--- src/main/python/palindromeNumber/PalindromeNumber.py
+++ src/main/python/palindromeNumber/PalindromeNumber.py
@@ -11,6 +11,15 @@ class Solution:
             idx += 1
         return True
 
+    def isPalindromeAnother(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reverted_number = 0
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
+            x //= 10
+        return x == reverted_number or x == reverted_number // 10
+
     def test(self):
         tests = [
             [121, True],
@@ -19,8 +28,8 @@ class Solution:
         ]
         for test in tests:
             x, expect_result = test
-            result = self.isPalindrome(x)
-            print(result == expect_result)
+            print(self.isPalindrome(x) == expect_result)
+            print(self.isPalindromeAnother(x) == expect_result)
 
 
 if __name__ == '__main__':
