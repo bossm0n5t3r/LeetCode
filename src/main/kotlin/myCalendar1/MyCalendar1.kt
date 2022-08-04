66d41f942d00862eaebbf028e2c075d0e242d6ae
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-04 21:00:00 +0900

    729. My Calendar I

--- src/main/kotlin/myCalendar1/MyCalendar1.kt
+++ src/main/kotlin/myCalendar1/MyCalendar1.kt
@@ -6,20 +6,11 @@ class MyCalendar1 {
     private val treeMap = TreeMap<Int, Int>()
 
     fun book(start: Int, end: Int): Boolean {
-        if (treeMap.containsKey(start)) return false
-        val upperStartKey = try {
-            treeMap.ceilingKey(start) ?: Int.MAX_VALUE
-        } catch (e: Exception) {
-            Int.MAX_VALUE
+        val lower = treeMap.lowerKey(end)
+        if (lower == null || treeMap[lower]!! <= start) {
+            treeMap[start] = end
+            return true
         }
-        val lowerEnd = try {
-            val lowerStartKey = treeMap.floorKey(start)
-            treeMap[lowerStartKey] ?: Int.MIN_VALUE
-        } catch (e: Exception) {
-            Int.MIN_VALUE
-        }
-        if (start < lowerEnd || upperStartKey < end) return false
-        treeMap[start] = end
-        return true
+        return false
     }
 }
