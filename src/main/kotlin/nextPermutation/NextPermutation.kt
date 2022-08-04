6cb4020855e4442b0482c733f0a033786a892f75
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-05 02:30:23 +0900

    31. Next Permutation

---
+++ src/main/kotlin/nextPermutation/NextPermutation.kt
@@ -0,0 +1,30 @@
+package nextPermutation
+
+class NextPermutation {
+    fun nextPermutation(nums: IntArray) {
+        var i = nums.size - 2
+        while (i >= 0 && nums[i + 1] <= nums[i]) i--
+        if (i >= 0) {
+            var j = nums.size - 1
+            while (nums[j] <= nums[i]) j--
+            swap(nums, i, j)
+        }
+        reversed(nums, i + 1)
+    }
+
+    private fun reversed(nums: IntArray, start: Int) {
+        var i = start
+        var j = nums.size - 1
+        while (i < j) {
+            swap(nums, i, j)
+            i++
+            j--
+        }
+    }
+
+    private fun swap(nums: IntArray, i: Int, j: Int) {
+        val tmp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = tmp
+    }
+}
---
+++ src/test/kotlin/nextPermutation/NextPermutationTest.kt
@@ -0,0 +1,53 @@
+package nextPermutation
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class NextPermutationTest {
+    private val sut = NextPermutation()
+
+    private data class NextPermutationTestData(
+        val nums: IntArray,
+        val result: IntArray,
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as NextPermutationTestData
+
+            if (!nums.contentEquals(other.nums)) return false
+            if (!result.contentEquals(other.result)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result1 = nums.contentHashCode()
+            result1 = 31 * result1 + result.contentHashCode()
+            return result1
+        }
+    }
+
+    @Test
+    fun nextPermutation() {
+        val tests = listOf(
+            NextPermutationTestData(
+                intArrayOf(1, 2, 3),
+                intArrayOf(1, 3, 2),
+            ),
+            NextPermutationTestData(
+                intArrayOf(3, 2, 1),
+                intArrayOf(1, 2, 3),
+            ),
+            NextPermutationTestData(
+                intArrayOf(1, 1, 5),
+                intArrayOf(1, 5, 1),
+            ),
+        )
+        tests.forEach { test ->
+            sut.nextPermutation(test.nums)
+            assertThat(test.nums).isEqualTo(test.result)
+        }
+    }
+}
