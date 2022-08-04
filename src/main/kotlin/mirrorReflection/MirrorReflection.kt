c2d567a00c223a2d15761f7033d92fe52ba6765c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-05 02:55:10 +0900

    858. Mirror Reflection

---
+++ src/main/kotlin/mirrorReflection/MirrorReflection.kt
@@ -0,0 +1,18 @@
+package mirrorReflection
+
+class MirrorReflection {
+    /**
+     * @link https://leetcode.com/problems/mirror-reflection/discuss/146336/Java-solution-with-an-easy-to-understand-explanation
+     */
+    fun mirrorReflection(p: Int, q: Int): Int {
+        var m = 1
+        var n = 1
+        while (m * p != n * q) {
+            n++
+            m = n * q / p
+        }
+        if (m % 2 == 0 && n % 2 == 1) return 0
+        if (m % 2 == 1 && n % 2 == 1) return 1
+        return if (m % 2 == 1 && n % 2 == 0) 2 else -1
+    }
+}
---
+++ src/main/kotlin/mirrorReflection/README.md
@@ -0,0 +1,4 @@
+# 858. Mirror Reflection
+
+- [https://leetcode.com/problems/mirror-reflection/](https://leetcode.com/problems/mirror-reflection/)
+  - [https://leetcode.com/problems/mirror-reflection/discuss/146336/Java-solution-with-an-easy-to-understand-explanation](https://leetcode.com/problems/mirror-reflection/discuss/146336/Java-solution-with-an-easy-to-understand-explanation)
