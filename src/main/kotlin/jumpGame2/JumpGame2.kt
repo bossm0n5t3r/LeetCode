d5e5e0195df12ce747849af7b95262893dfdafb1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-06 03:27:40 +0900

    45. Jump Game II

--- src/main/kotlin/jumpGame2/JumpGame2.kt
+++ src/main/kotlin/jumpGame2/JumpGame2.kt
@@ -1,27 +1,18 @@
 package jumpGame2
 
-import java.util.LinkedList
-
 class JumpGame2 {
     fun jump(nums: IntArray): Int {
-        val jump = IntArray(nums.size) { 0 }
-        val visited = BooleanArray(nums.size) { false }
-        val queue = LinkedList<Int>()
-        queue.add(0)
-        visited[0] = true
-        while (queue.isNotEmpty()) {
-            val curPos = queue.poll()
-            val curJump = jump[curPos]
-            val maxJumpLength = nums[curPos]
-            (0..maxJumpLength).forEach { i ->
-                val nextPos = curPos + i
-                if (nextPos < nums.size && !visited[nextPos]) {
-                    visited[nextPos] = true
-                    jump[nextPos] = curJump + 1
-                    queue.add(nextPos)
+        var position = nums.size - 1
+        var steps = 0
+        while (position != 0) {
+            for (i in 0 until position) {
+                if (nums[i] >= position - i) {
+                    position = i
+                    steps++
+                    break
                 }
             }
         }
-        return jump[nums.size - 1]
+        return steps
     }
 }
