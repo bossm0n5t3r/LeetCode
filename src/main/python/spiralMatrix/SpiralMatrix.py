dd07e66b3822a8750651f45b9fb7979a3ed5b24f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-08-10 00:53:03 +0900

    54. Spiral Matrix

--- src/main/python/spiralMatrix/SpiralMatrix.py
+++ src/main/python/spiralMatrix/SpiralMatrix.py
@@ -1,37 +1,10 @@
 class Solution:
+    """
+    https://leetcode.com/problems/spiral-matrix/discuss/20571/1-liner-in-Python-+-Ruby
+    """
+
     def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
-        m, n = len(matrix), len(matrix[0])
-        given = [item for row in matrix for item in row]
-        visited = [[False] * n for _ in range(m)]
-        result = []
-        idx = 1
-        # direction
-        # 동, 서, 남, 북 = 0, 1, 2, 3
-        r, c, direction = 0, 0, 0
-        while idx <= m * n:
-            result.append(given[r * n + c])
-            visited[r][c] = True
-            if idx == m * n:
-                break
-            idx += 1
-            if direction == 0 and (c == n - 1 or visited[r][c + 1]):
-                r, direction = r + 1, 2
-            elif direction == 2 and (r == m - 1 or visited[r + 1][c]):
-                c, direction = c - 1, 1
-            elif direction == 1 and (c == 0 or visited[r][c - 1]):
-                r, direction = r - 1, 3
-            elif direction == 3 and (r == 0 or visited[r - 1][c]):
-                c, direction = c + 1, 0
-            else:
-                if direction == 0:
-                    c += 1
-                elif direction == 1:
-                    c -= 1
-                elif direction == 2:
-                    r += 1
-                elif direction == 3:
-                    r -= 1
-        return result
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
     def test(self):
         tests = [
