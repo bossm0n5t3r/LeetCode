a5ebeeb86c866f1d18e0a1572171a8276b046cfa
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-13 23:45:00 +0900

    fix: Edit build.gradle.kts
    - Update kotlin version
    - Update gradle ktlint version
    - Update test libraries version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
+    kotlin("jvm") version "1.8.22"
+    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
 }
 
 repositories {
@@ -15,8 +15,8 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
@@ -26,4 +26,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+}
+
+ktlint {
+    version.set("0.49.1")
 }
