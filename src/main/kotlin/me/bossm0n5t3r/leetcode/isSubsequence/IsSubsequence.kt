9f962795d7a07b976c18450481672c5a63fe8e68
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-09 20:00:10 +0900

    392. Is Subsequence

--- src/main/kotlin/me/bossm0n5t3r/leetcode/isSubsequence/IsSubsequence.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/isSubsequence/IsSubsequence.kt
@@ -3,18 +3,23 @@ package me.bossm0n5t3r.leetcode.isSubsequence
 class IsSubsequence {
     class Solution {
         fun isSubsequence(s: String, t: String): Boolean {
-            if (s.isEmpty()) return true
-            val sChars = s.toSet()
-            val tChars = t.toSet()
-            if (tChars.containsAll(sChars).not()) return false
+            val sLength = s.length
+            val tLength = t.length
+
+            if (sLength > tLength) return false
 
             var sIdx = 0
-            for (tChar in t) {
-                if (tChar != s[sIdx]) continue
-                sIdx++
-                if (sIdx == s.length) return true
+            var tIdx = 0
+            while (sIdx < sLength) {
+                if (tIdx == tLength) return false
+                if (s[sIdx] == t[tIdx]) {
+                    sIdx++
+                    tIdx++
+                } else {
+                    tIdx++
+                }
             }
-            return false
+            return true
         }
     }
 }
