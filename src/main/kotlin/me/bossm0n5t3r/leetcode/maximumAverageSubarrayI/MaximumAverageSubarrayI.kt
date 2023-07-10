4ffed0cb2e7df7ef6b9d96cf7c6c40bedb18b85f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-10 22:39:20 +0900

    643. Maximum Average Subarray I

--- src/main/kotlin/me/bossm0n5t3r/leetcode/maximumAverageSubarrayI/MaximumAverageSubarrayI.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/maximumAverageSubarrayI/MaximumAverageSubarrayI.kt
@@ -3,16 +3,18 @@ package me.bossm0n5t3r.leetcode.maximumAverageSubarrayI
 class MaximumAverageSubarrayI {
     class Solution {
         fun findMaxAverage(nums: IntArray, k: Int): Double {
-            var tmp = nums.take(k).sum().toDouble()
+            var tmp = 0.0
+            for (i in 0 until k) {
+                tmp += nums[i]
+            }
+
             var maxAverage = tmp / k
-            for (i in 0 until nums.size - k) {
+            for (i in k until nums.size) {
+                tmp += nums[i]
+                tmp -= nums[i - k]
                 val tmpAverage = tmp / k
                 if (tmpAverage > maxAverage) maxAverage = tmpAverage
-                tmp -= nums[i]
-                tmp += nums[i + k]
             }
-            val tmpAverage = tmp / k
-            if (tmpAverage > maxAverage) maxAverage = tmpAverage
             return maxAverage
         }
     }
