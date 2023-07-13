dbb00d0410eecac26c5508351e93df3e1b39aef6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-14 01:30:00 +0900

    1493. Longest Subarray of 1's After Deleting One Element

--- src/main/kotlin/me/bossm0n5t3r/leetcode/longestSubarrayOf1SAfterDeletingOneElement/LongestSubarrayOf1SAfterDeletingOneElement.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/longestSubarrayOf1SAfterDeletingOneElement/LongestSubarrayOf1SAfterDeletingOneElement.kt
@@ -3,40 +3,21 @@ package me.bossm0n5t3r.leetcode.longestSubarrayOf1SAfterDeletingOneElement
 class LongestSubarrayOf1SAfterDeletingOneElement {
     class Solution {
         fun longestSubarray(nums: IntArray): Int {
-            var tmp = 0
-            var removeZero = true
-            var idx = 0
-            while (idx < nums.size) {
-                if (nums[idx] == 1) {
-                    tmp++
-                } else if (removeZero) {
-                    tmp++
-                    removeZero = false
-                } else {
-                    break
-                }
-                idx++
-            }
-            var (s, e) = 0 to idx
-            var result = tmp
+            var (s, e) = 0 to 0
+            var deleteOnce = 1
             while (e < nums.size) {
-                if (nums[e] == 1) {
-                    tmp++
-                    while (e + 1 < nums.size && nums[e + 1] == 1) {
-                        e++
-                        tmp++
-                    }
-                } else {
-                    while (s < e && nums[s] == 1) {
-                        s++
-                        tmp--
+                if (nums[e] == 0) {
+                    deleteOnce--
+                }
+                if (deleteOnce < 0) {
+                    if (nums[s] == 0) {
+                        deleteOnce++
                     }
                     s++
                 }
-                if (tmp > result) result = tmp
                 e++
             }
-            return result - 1
+            return e - s - 1
         }
     }
 }
