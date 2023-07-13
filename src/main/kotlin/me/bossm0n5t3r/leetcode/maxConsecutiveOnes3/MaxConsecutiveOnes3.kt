5045ec6ffbc39346378490e078787fedf97a162a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-14 01:00:00 +0900

    1004. Max Consecutive Ones III

--- src/main/kotlin/me/bossm0n5t3r/leetcode/maxConsecutiveOnes3/MaxConsecutiveOnes3.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/maxConsecutiveOnes3/MaxConsecutiveOnes3.kt
@@ -3,40 +3,21 @@ package me.bossm0n5t3r.leetcode.maxConsecutiveOnes3
 class MaxConsecutiveOnes3 {
     class Solution {
         fun longestOnes(nums: IntArray, k: Int): Int {
-            var tmp = 0
-            var tmpK = k
-            var idx = 0
-            while (idx < nums.size) {
-                if (nums[idx] == 1) {
-                    tmp++
-                } else if (tmpK > 0) {
-                    tmp++
-                    tmpK--
-                } else {
-                    break
-                }
-                idx++
-            }
-            var (s, e) = 0 to idx
-            var result = tmp
+            var (s, e) = 0 to 0
+            var flipCount = k
             while (e < nums.size) {
-                if (nums[e] == 1) {
-                    tmp++
-                    while (e + 1 < nums.size && nums[e + 1] == 1) {
-                        e++
-                        tmp++
-                    }
-                } else {
-                    while (s < e && nums[s] == 1) {
-                        s++
-                        tmp--
+                if (nums[e] == 0) {
+                    flipCount--
+                }
+                if (flipCount < 0) {
+                    if (nums[s] == 0) {
+                        flipCount++
                     }
                     s++
                 }
-                if (tmp > result) result = tmp
                 e++
             }
-            return result
+            return e - s
         }
     }
 }
