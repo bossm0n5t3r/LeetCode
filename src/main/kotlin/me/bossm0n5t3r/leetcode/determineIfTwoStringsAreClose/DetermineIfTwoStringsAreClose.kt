a1cadee659d26bde62a0cc20dd5a0ab12c04c4c1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-15 07:03:01 +0900

    1657. Determine if Two Strings Are Close

--- src/main/kotlin/me/bossm0n5t3r/leetcode/determineIfTwoStringsAreClose/DetermineIfTwoStringsAreClose.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/determineIfTwoStringsAreClose/DetermineIfTwoStringsAreClose.kt
@@ -3,18 +3,10 @@ package me.bossm0n5t3r.leetcode.determineIfTwoStringsAreClose
 class DetermineIfTwoStringsAreClose {
     class Solution {
         fun closeStrings(word1: String, word2: String): Boolean {
-            val word1Map = getCharToCount(word1)
-            val word2Map = getCharToCount(word2)
+            val word1Map = word1.groupingBy { it }.eachCount()
+            val word2Map = word2.groupingBy { it }.eachCount()
 
             return (word1Map.keys == word2Map.keys && word1Map.values.sorted() == word2Map.values.sorted())
-        }
-
-        private fun getCharToCount(word: String): MutableMap<Char, Int> {
-            val result = mutableMapOf<Char, Int>()
-            for (c in word) {
-                result[c] = result.getOrDefault(c, 0) + 1
-            }
-            return result
         }
     }
 }
