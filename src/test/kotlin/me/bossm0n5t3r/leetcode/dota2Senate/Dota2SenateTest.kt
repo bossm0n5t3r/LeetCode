9868c849c5042986602c03f1e3384a6859a8dc68
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-19 00:49:23 +0900

    649. Dota2 Senate

--- src/test/kotlin/me/bossm0n5t3r/leetcode/dota2Senate/Dota2SenateTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/leetcode/dota2Senate/Dota2SenateTest.kt
@@ -5,12 +5,12 @@ import kotlin.test.assertEquals
 
 class Dota2SenateTest {
     private val sut = Dota2Senate.Solution()
-    
+
     private data class TestData(
         val senate: String,
         val result: String,
     )
-    
+
     @Test
     fun test() {
         val tests = listOf(
@@ -23,7 +23,7 @@ class Dota2SenateTest {
                 "Dire",
             ),
         )
-        
+
         tests.forEach { test ->
             assertEquals(
                 sut.predictPartyVictory(test.senate),
