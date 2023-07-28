61219bdb218acbde2168092ebd39895dbdb05fea
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-28 22:20:01 +0900

    1679. Max Number of K-Sum Pairs

--- src/main/kotlin/me/bossm0n5t3r/leetcode/maxNumberOfKSumPairs/MaxNumberOfKSumPairs.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/maxNumberOfKSumPairs/MaxNumberOfKSumPairs.kt
@@ -1,47 +1,22 @@
 package me.bossm0n5t3r.leetcode.maxNumberOfKSumPairs
 
-import kotlin.math.min
-
 class MaxNumberOfKSumPairs {
     class Solution {
         fun maxOperations(nums: IntArray, k: Int): Int {
-            val numToCount = getNumToCount(nums)
-//            val numToCount = nums
-//                .toList()
-//                .groupingBy { it }
-//                .eachCount()
-//                .toMutableMap()
-            val sortedKeys = numToCount.keys.sorted()
+            val numToCount = mutableMapOf<Int, Int>()
             var operations = 0
-            for (key in sortedKeys) {
-                if (numToCount.containsKey(k - key)) {
-                    val thisCount = numToCount[key]!!
-                    val otherCount = numToCount[k - key]!!
-                    if (
-                        thisCount > 0 &&
-                        otherCount > 0
-                    ) {
-                        val minCount = if (2 * key == k) {
-                            thisCount / 2
-                        } else {
-                            min(thisCount, otherCount)
-                        }
-                        operations += minCount
-                        numToCount[key] = numToCount[key]!! - minCount
-                        numToCount[k - key] = numToCount[k - key]!! - minCount
-                    }
+            for (num in nums) {
+                if (num > k) continue
+                val diff = k - num
+                val diffCount = numToCount[diff] ?: 0
+                if (diffCount > 0) {
+                    numToCount[diff] = diffCount - 1
+                    operations++
+                } else {
+                    numToCount[num] = (numToCount[num] ?: 0) + 1
                 }
-                if (2 * key == k) break
             }
             return operations
-        }
-
-        private fun getNumToCount(nums: IntArray): MutableMap<Int, Int> {
-            val result = mutableMapOf<Int, Int>()
-            for (num in nums) {
-                result[num] = result.getOrDefault(num, 0) + 1
-            }
-            return result
         }
     }
 }
