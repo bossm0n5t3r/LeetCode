719311064e8f02e50a70c132a48a5a71a520db48
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-04 23:30:30 +0900

    fix: Edit StringUtil
    - Add String.toIntArray
    - Add String.toArrayOfCharArray

--- src/main/kotlin/me/bossm0n5t3r/leetcode/utils/StringUtil.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/utils/StringUtil.kt
@@ -14,4 +14,28 @@ object StringUtil {
             }
             .toTypedArray()
     }
+
+    fun String.toIntArray(): IntArray {
+        return this
+            .drop(1)
+            .dropLast(1)
+            .split(",")
+            .map { it.toInt() }
+            .toIntArray()
+    }
+
+    fun String.toArrayOfCharArray(): Array<CharArray> {
+        return this
+            .drop(2)
+            .dropLast(2)
+            .split("],[")
+            .map {
+                it
+                    .replace("\"", "")
+                    .split(",")
+                    .map { c -> c.first() }
+                    .toCharArray()
+            }
+            .toTypedArray()
+    }
 }
