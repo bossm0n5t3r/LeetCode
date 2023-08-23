1436e86d5e5050670f2205253300fc11e88e9e7d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-24 03:41:41 +0900

    875. Koko Eating Bananas

---
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/kokoEatingBananas/KokoEatingBananas.kt
@@ -0,0 +1,45 @@
+package me.bossm0n5t3r.leetcode.kokoEatingBananas
+
+class KokoEatingBananas {
+    class Solution {
+        fun minEatingSpeed(piles: IntArray, h: Int): Int {
+            val maxPile = binarySearchForFindingMax(piles, 0, piles.size - 1)
+            if (piles.size == h) return maxPile
+            var start = 0
+            var end = maxPile
+            while (start < end) {
+                println("start: $start, end: $end")
+                val mid = (start + end) / 2
+                if (mid == 0) break
+                if (isValid(piles, h, mid)) {
+                    end = mid
+                } else {
+                    start = mid + 1
+                }
+            }
+            return if (start == 0) 1 else start
+        }
+
+        @Suppress("ReturnCount")
+        private fun binarySearchForFindingMax(piles: IntArray, start: Int, end: Int): Int {
+            if (start > end) return -1
+            if (start == end) return piles[start]
+            if (start + 1 == end) return maxOf(piles[start], piles[end])
+            val mid = (start + end) / 2
+
+            return maxOf(
+                binarySearchForFindingMax(piles, start, mid),
+                binarySearchForFindingMax(piles, mid + 1, end),
+            )
+        }
+
+        private fun isValid(piles: IntArray, h: Int, k: Int): Boolean {
+            var hours = 0L
+            for (pile in piles) {
+                hours += pile / k
+                if (pile % k != 0) hours++
+            }
+            return hours <= h
+        }
+    }
+}
---
+++ src/test/kotlin/me/bossm0n5t3r/leetcode/kokoEatingBananas/KokoEatingBananasTest.kt
@@ -0,0 +1,73 @@
+package me.bossm0n5t3r.leetcode.kokoEatingBananas
+
+import me.bossm0n5t3r.leetcode.utils.StringUtil.toIntArray
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class KokoEatingBananasTest {
+    private val sut = KokoEatingBananas.Solution()
+
+    private data class TestData(
+        val piles: IntArray,
+        val h: Int,
+        val result: Int,
+    ) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as TestData
+
+            if (!piles.contentEquals(other.piles)) return false
+            if (h != other.h) return false
+            if (result != other.result) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result1 = piles.contentHashCode()
+            result1 = 31 * result1 + h
+            result1 = 31 * result1 + result
+            return result1
+        }
+    }
+
+    @Test
+    fun test() {
+        val tests = listOf(
+            TestData(
+                piles = "[3,6,7,11]".toIntArray(),
+                h = 8,
+                result = 4,
+            ),
+            TestData(
+                piles = "[30,11,23,4,20]".toIntArray(),
+                h = 5,
+                result = 30,
+            ),
+            TestData(
+                piles = "[30,11,23,4,20]".toIntArray(),
+                h = 6,
+                result = 23,
+            ),
+            TestData(
+                piles = "[3,6,7,11]".toIntArray(),
+                h = 18,
+                result = 2,
+            ),
+            TestData(
+                piles = "[805306368,805306368,805306368]".toIntArray(),
+                h = 1000000000,
+                result = 3,
+            ),
+        )
+
+        tests.forEach { test ->
+            assertEquals(
+                sut.minEatingSpeed(test.piles, test.h),
+                test.result,
+            )
+        }
+    }
+}
