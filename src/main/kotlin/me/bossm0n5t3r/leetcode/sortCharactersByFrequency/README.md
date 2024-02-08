d4c1e4f87f018296ad2e70053dd095d7763765e6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-08 10:40:45 +0900

    451. Sort Characters By Frequency

---
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/sortCharactersByFrequency/README.md
@@ -0,0 +1,3 @@
+# 451. Sort Characters By Frequency
+
+- [https://leetcode.com/problems/sort-characters-by-frequency/description/](https://leetcode.com/problems/sort-characters-by-frequency/description/)
---
+++ src/test/kotlin/me/bossm0n5t3r/leetcode/sortCharactersByFrequency/SortCharactersByFrequencyTest.kt
@@ -0,0 +1,30 @@
+package me.bossm0n5t3r.leetcode.sortCharactersByFrequency
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class SortCharactersByFrequencyTest {
+    private val sut = SortCharactersByFrequency.Solution()
+
+    private data class TestData(
+        val s: String,
+        val result: String,
+    )
+
+    @Test
+    fun test() {
+        val tests =
+            listOf(
+                TestData("tree", "eert"),
+                TestData("cccaaa", "aaaccc"),
+                TestData("Aabb", "bbAa"),
+            )
+
+        tests.forEach { test ->
+            assertEquals(
+                test.result,
+                sut.frequencySort(test.s),
+            )
+        }
+    }
+}
