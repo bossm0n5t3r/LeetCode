efa178d5c227ecff3d47b3fa15636360344bce02
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-12 21:53:40 +0900

    169. Majority Element

--- src/main/kotlin/me/bossm0n5t3r/leetcode/majorityElement/MajorityElement.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/majorityElement/MajorityElement.kt
@@ -4,12 +4,12 @@ class MajorityElement {
     class Solution {
         fun majorityElement(nums: IntArray): Int {
             var count = 0
-            var result: Int? = null
+            var result = 0
             for (num in nums) {
                 if (count == 0) result = num
                 count += if (result == num) 1 else -1
             }
-            return result!!
+            return result
         }
     }
 }
