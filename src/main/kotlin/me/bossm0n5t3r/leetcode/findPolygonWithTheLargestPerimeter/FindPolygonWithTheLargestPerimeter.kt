eb9d02652c0f451d67a5ff53ffa7b7274a36996a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-16 04:59:19 +0900

    2971. Find Polygon With the Largest Perimeter

--- src/main/kotlin/me/bossm0n5t3r/leetcode/findPolygonWithTheLargestPerimeter/FindPolygonWithTheLargestPerimeter.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/findPolygonWithTheLargestPerimeter/FindPolygonWithTheLargestPerimeter.kt
@@ -1,17 +1,23 @@
 package me.bossm0n5t3r.leetcode.findPolygonWithTheLargestPerimeter
 
+import java.util.PriorityQueue
+
 class FindPolygonWithTheLargestPerimeter {
     class Solution {
         fun largestPerimeter(nums: IntArray): Long {
-            val longNums = nums.map { it.toLong() }
-            val sortedLongNums = longNums.sortedDescending()
-            var sumLongNums = longNums.sum()
-            for (i in sortedLongNums.indices) {
-                val cur = sortedLongNums[i]
-                if (sumLongNums - cur > cur && sortedLongNums.size - i >= 2) {
-                    return sumLongNums
-                }
-                sumLongNums -= cur
+            val pq = PriorityQueue<Long> { a, b -> b.compareTo(a) }
+            var result = 0L
+            var numOfNums = 0
+            for (num in nums) {
+                pq.add(num.toLong())
+                result += num.toLong()
+                numOfNums++
+            }
+            while (pq.isNotEmpty()) {
+                val cur = pq.peek()
+                if (result - cur > cur && numOfNums >= 3) return result
+                result -= pq.poll()
+                numOfNums--
             }
             return -1
         }
