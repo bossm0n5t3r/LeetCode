57e4a12fbb2be4d9c46af5ae191f47fab4ed75d9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-20 23:30:00 +0900

    268. Missing Number

--- src/main/kotlin/me/bossm0n5t3r/leetcode/missingNumber/MissingNumber.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/missingNumber/MissingNumber.kt
@@ -3,12 +3,7 @@ package me.bossm0n5t3r.leetcode.missingNumber
 class MissingNumber {
     class Solution {
         fun missingNumber(nums: IntArray): Int {
-            val n = nums.size
-            var result = n * (n + 1) / 2
-            for (num in nums) {
-                result -= num
-            }
-            return result
+            return (nums.size * (nums.size + 1) / 2) - nums.sum()
         }
     }
 }
