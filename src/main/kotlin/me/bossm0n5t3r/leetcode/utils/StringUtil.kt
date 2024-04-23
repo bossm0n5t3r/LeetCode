aae77ba9f7c2e844116c8faa3c94a631360375a5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-04-23 23:49:00 +0900

    fix: Update String.toArrayOfIntArray
    - Edit StringUtil

--- src/main/kotlin/me/bossm0n5t3r/leetcode/utils/StringUtil.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/utils/StringUtil.kt
@@ -2,17 +2,21 @@ package me.bossm0n5t3r.leetcode.utils
 
 object StringUtil {
     fun String.toArrayOfIntArray(): Array<IntArray> {
-        return this
-            .drop(2)
-            .dropLast(2)
-            .split("],[")
-            .map {
-                it
-                    .split(",")
-                    .map { c -> c.toInt() }
-                    .toIntArray()
-            }
-            .toTypedArray()
+        return try {
+            this
+                .drop(2)
+                .dropLast(2)
+                .split("],[")
+                .map {
+                    it
+                        .split(",")
+                        .map { c -> c.toInt() }
+                        .toIntArray()
+                }
+                .toTypedArray()
+        } catch (e: Exception) {
+            emptyArray()
+        }
     }
 
     fun String.toIntArray(): IntArray {
