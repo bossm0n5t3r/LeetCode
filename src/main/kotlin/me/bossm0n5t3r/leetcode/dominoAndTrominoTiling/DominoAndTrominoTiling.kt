ea76cafc60de3c16996343e80a724c15a54bebe7
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-05-02 22:08:00 +0900

    790. Domino and Tromino Tiling

--- src/main/kotlin/me/bossm0n5t3r/leetcode/dominoAndTrominoTiling/DominoAndTrominoTiling.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/dominoAndTrominoTiling/DominoAndTrominoTiling.kt
@@ -1,26 +1,21 @@
 package me.bossm0n5t3r.leetcode.dominoAndTrominoTiling
 
-import java.math.BigInteger
-
 class DominoAndTrominoTiling {
     class Solution {
         fun numTilings(n: Int): Int {
             if (n == 1) return 1
             if (n == 2) return 2
 
-            val modulo = BigInteger.valueOf(1_000_000_007)
-            val dp =
-                Array<BigInteger>(n + 1) { BigInteger.ZERO }
-                    .also {
-                        it[0] = BigInteger.ONE
-                        it[1] = BigInteger.ONE
-                        it[2] = BigInteger.valueOf(2)
-                    }
+            val modulo = 1_000_000_007L
+            val dp = LongArray(n + 1)
+            dp[0] = 1L
+            dp[1] = 1L
+            dp[2] = 2L
             for (i in 3..n) {
-                dp[i] = dp[i - 1].times(BigInteger.valueOf(2)).plus(dp[i - 3])
+                dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % modulo
             }
 
-            return dp[n].mod(modulo).toInt()
+            return dp[n].toInt()
         }
     }
 }
