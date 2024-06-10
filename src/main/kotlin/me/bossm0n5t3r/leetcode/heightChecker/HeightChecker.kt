cb2356e1ccdb694fc81cd6df566661411450f172
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-06-10 23:33:10 +0900

    1051. Height Checker

--- src/main/kotlin/me/bossm0n5t3r/leetcode/heightChecker/HeightChecker.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/heightChecker/HeightChecker.kt
@@ -3,7 +3,31 @@ package me.bossm0n5t3r.leetcode.heightChecker
 class HeightChecker {
     class Solution {
         fun heightChecker(heights: IntArray): Int {
-            return heights.zip(heights.sorted()).count { it.first != it.second }
+            val heightsSize = heights.size
+            var prefixSum = 0
+
+            for (i in 0 until heightsSize) {
+                prefixSum = maxOf(prefixSum, heights[i])
+            }
+
+            val countArray = IntArray(prefixSum + 1)
+
+            for (i in 0 until heightsSize) {
+                countArray[heights[i]]++
+            }
+
+            for (i in 1..prefixSum) {
+                countArray[i] += countArray[i - 1]
+            }
+
+            val outputArray = IntArray(heightsSize)
+
+            for (i in heightsSize - 1 downTo 0) {
+                outputArray[countArray[heights[i]] - 1] = heights[i]
+                countArray[heights[i]]--
+            }
+
+            return heights.zip(outputArray).count { it.first != it.second }
         }
     }
 }
