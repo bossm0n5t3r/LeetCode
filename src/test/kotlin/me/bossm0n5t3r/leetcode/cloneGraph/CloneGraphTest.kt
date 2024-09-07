78ff815aaf52d07f0cc7d32251d51db99c5bab8b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-09-07 23:00:00 +0900

    133. Clone Graph

--- src/test/kotlin/me/bossm0n5t3r/leetcode/cloneGraph/CloneGraphTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/leetcode/cloneGraph/CloneGraphTest.kt
@@ -1,24 +1,31 @@
 package me.bossm0n5t3r.leetcode.cloneGraph
 
 import me.bossm0n5t3r.leetcode.utils.Node
+import me.bossm0n5t3r.leetcode.utils.NodeUtil
+import me.bossm0n5t3r.leetcode.utils.NodeUtil.toList
+import me.bossm0n5t3r.leetcode.utils.StringUtil.toListOfIntList
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class CloneGraphTest {
     private val sut = CloneGraph.Solution()
 
-    private data class TestData(val node: Node?, val result: Node?)
+    private data class TestData(val node: Node?)
 
     @Test
     fun test() {
-//        TODO("Not yet implemented")
-//        val tests = listOf(
-//            TestData(),
-//        )
-//
-//        tests.forEach { test ->
-//            assertEquals(
-//
-//            )
-//        }
+        val tests =
+            listOf(
+                TestData(NodeUtil.generateNodeOrNull("[[2,4],[1,3],[2,4],[1,3]]".toListOfIntList())),
+                TestData(NodeUtil.generateNodeOrNull("[[]]".toListOfIntList())),
+                TestData(NodeUtil.generateNodeOrNull("[]".toListOfIntList())),
+            )
+
+        tests.forEach { test ->
+            assertEquals(
+                test.node?.toList(),
+                sut.cloneGraph(test.node)?.toList(),
+            )
+        }
     }
 }
