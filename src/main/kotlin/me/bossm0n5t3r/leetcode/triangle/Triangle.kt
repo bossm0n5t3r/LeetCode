df6e61f69cb59799fe9e589d4b28e3958c8ce000
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-01-04 19:53:00 +0900

    120. Triangle

--- src/main/kotlin/me/bossm0n5t3r/leetcode/triangle/Triangle.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/triangle/Triangle.kt
@@ -4,6 +4,19 @@ import kotlin.math.min
 
 class Triangle {
     class Solution {
+        fun minimumTotalAt20250104(triangle: List<List<Int>>): Int {
+            if (triangle.size == 1) return triangle[0][0]
+            val dp = IntArray(triangle.size) { triangle[triangle.size - 1][it] }
+
+            for (i in triangle.size - 2 downTo 0) {
+                for (j in 0..i) {
+                    dp[j] = triangle[i][j] + minOf(dp[j], dp[j + 1])
+                }
+            }
+
+            return dp[0]
+        }
+
         fun minimumTotal(triangle: List<List<Int>>): Int {
             val n = triangle.size
             val cache = IntArray(n) { 0 }
