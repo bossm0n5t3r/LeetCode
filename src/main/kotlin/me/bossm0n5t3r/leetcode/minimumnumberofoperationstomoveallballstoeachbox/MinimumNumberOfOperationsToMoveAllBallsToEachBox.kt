29d73a31bc19ffb843117b59d3f2b1a60c7925be
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-01-07 23:30:10 +0900

    1769. Minimum Number of Operations to Move All Balls to Each Box

--- src/main/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofoperationstomoveallballstoeachbox/MinimumNumberOfOperationsToMoveAllBallsToEachBox.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofoperationstomoveallballstoeachbox/MinimumNumberOfOperationsToMoveAllBallsToEachBox.kt
@@ -3,20 +3,26 @@ package me.bossm0n5t3r.leetcode.minimumnumberofoperationstomoveallballstoeachbox
 class MinimumNumberOfOperationsToMoveAllBallsToEachBox {
     class Solution {
         fun minOperations(boxes: String): IntArray {
-            val result = IntArray(boxes.length)
-            for (i in boxes.indices) {
-                val box = boxes[i]
-                if (box == '1') {
-                    for (j in boxes.indices) {
-                        if (j != i) {
-                            result[j] += (j - i).abs()
-                        }
-                    }
-                }
+            val n = boxes.length
+            val result = IntArray(n)
+            val left2Right = IntArray(n)
+            val right2Left = IntArray(n)
+
+            var left2RightCount = if (boxes[0] == '1') 1 else 0
+            for (i in 1 until n) {
+                left2Right[i] = left2Right[i - 1] + left2RightCount
+                result[i] += left2Right[i]
+                left2RightCount += if (boxes[i] == '1') 1 else 0
             }
+
+            var right2LeftCount = if (boxes[n - 1] == '1') 1 else 0
+            for (i in n - 2 downTo 0) {
+                right2Left[i] = right2Left[i + 1] + right2LeftCount
+                result[i] += right2Left[i]
+                right2LeftCount += if (boxes[i] == '1') 1 else 0
+            }
+
             return result
         }
-
-        private fun Int.abs() = if (this >= 0) this else -this
     }
 }
