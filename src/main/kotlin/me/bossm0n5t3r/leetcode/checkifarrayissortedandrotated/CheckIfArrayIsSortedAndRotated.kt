765add0134795d5ddc6a5c0df9e7fc08450e926c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-05 00:30:00 +0900

    1752. Check if Array Is Sorted and Rotated

--- src/main/kotlin/me/bossm0n5t3r/leetcode/checkifarrayissortedandrotated/CheckIfArrayIsSortedAndRotated.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/checkifarrayissortedandrotated/CheckIfArrayIsSortedAndRotated.kt
@@ -3,33 +3,13 @@ package me.bossm0n5t3r.leetcode.checkifarrayissortedandrotated
 class CheckIfArrayIsSortedAndRotated {
     class Solution {
         fun check(nums: IntArray): Boolean {
-            val numsList = nums.toList()
-            val minimumIndex = findMinimumIndex(numsList)
-            return numsList.rotate(minimumIndex) == numsList.sorted()
-        }
-
-        private fun findMinimumIndex(nums: List<Int>): Int {
-            val minimum = nums.minOrNull() ?: error("Not found minimum")
-            var minimumIndex = nums.indexOfFirst { it == minimum }
-            if (minimumIndex == 0) {
-                var nextMinimumIndex = Int.MIN_VALUE
-                var isFirst = true
-                for (i in 1 until nums.size) {
-                    if (isFirst.not() && nums[i] == minimum) {
-                        nextMinimumIndex = i
-                        break
-                    }
-                    if (nums[i] != minimum) {
-                        isFirst = false
-                    }
-                }
-                if (nextMinimumIndex != Int.MIN_VALUE) {
-                    minimumIndex = nextMinimumIndex
+            var count = 0
+            for (i in nums.indices) {
+                if (nums[i] > nums[(i + 1) % nums.size]) {
+                    count++
                 }
             }
-            return minimumIndex
+            return count <= 1
         }
-
-        private fun List<Int>.rotate(n: Int): List<Int> = drop(n) + take(n)
     }
 }
