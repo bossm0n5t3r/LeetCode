e91efeb5d696a97b31ce3816b65ef6ffbbbe94c7
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-04 23:25:00 +0900

    1800. Maximum Ascending Subarray Sum

--- src/main/kotlin/me/bossm0n5t3r/leetcode/maximumascendingsubarraysum/MaximumAscendingSubarraySum.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/maximumascendingsubarraysum/MaximumAscendingSubarraySum.kt
@@ -3,23 +3,15 @@ package me.bossm0n5t3r.leetcode.maximumascendingsubarraysum
 class MaximumAscendingSubarraySum {
     class Solution {
         fun maxAscendingSum(nums: IntArray): Int {
-            var result = 0
-            for (i in nums.indices) {
-                result = maxOf(result, findMaxAscendingSum(nums, i))
-            }
-            return result
-        }
-
-        private fun findMaxAscendingSum(
-            nums: IntArray,
-            index: Int,
-        ): Int {
-            var result = 0
-            var prev = 0
-            for (i in index until nums.size) {
-                if (nums[i] <= prev) return result
-                prev = nums[i]
-                result += prev
+            var result = nums[0]
+            var currentSum = nums[0]
+            for (i in 1 until nums.size) {
+                if (nums[i] > nums[i - 1]) {
+                    currentSum += nums[i]
+                } else {
+                    currentSum = nums[i]
+                }
+                result = maxOf(result, currentSum)
             }
             return result
         }
