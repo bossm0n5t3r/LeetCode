14744294460ef3515ef6bbd65eab74414432a55d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-09 23:30:00 +0900

    2379. Minimum Recolors to Get K Consecutive Black Blocks

--- src/main/kotlin/me/bossm0n5t3r/leetcode/minimumrecolorstogetkconsecutiveblackblocks/MinimumRecolorsToGetKConsecutiveBlackBlocks.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/minimumrecolorstogetkconsecutiveblackblocks/MinimumRecolorsToGetKConsecutiveBlackBlocks.kt
@@ -1,20 +1,34 @@
 package me.bossm0n5t3r.leetcode.minimumrecolorstogetkconsecutiveblackblocks
 
+import java.util.LinkedList
+import java.util.Queue
+
 class MinimumRecolorsToGetKConsecutiveBlackBlocks {
     class Solution {
         fun minimumRecolors(
             blocks: String,
             k: Int,
         ): Int {
-            val prefixSum = blocks.runningFold(0) { acc: Int, c: Char -> if (c == 'W') acc + 1 else acc }.drop(1)
-            var result = prefixSum.last()
-            for (i in blocks.lastIndex downTo k) {
-                if (result > prefixSum[i] - prefixSum[i - k]) {
-                    result = prefixSum[i] - prefixSum[i - k]
-                }
+            val queue: Queue<Char> = LinkedList()
+            var numWhites = 0
+            repeat(k) {
+                val cur = blocks[it]
+                if (cur == 'W') numWhites++
+                queue.offer(cur)
             }
-            result = minOf(result, prefixSum[k - 1])
-            return result
+
+            var numRecolors = numWhites
+            for (i in k until blocks.length) {
+                if (queue.poll() == 'W') numWhites--
+
+                val cur = blocks[i]
+                if (cur == 'W') numWhites++
+                queue.offer(cur)
+
+                numRecolors = minOf(numRecolors, numWhites)
+            }
+
+            return numRecolors
         }
     }
 }
