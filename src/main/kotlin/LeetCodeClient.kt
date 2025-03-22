8f9b8a5f6f2a0aaa7b3c981ef257b8c5936898a1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-22 19:07:00 +0900

    feat: Update LeetCodeClient

--- src/main/kotlin/LeetCodeClient.kt
+++ src/main/kotlin/LeetCodeClient.kt
@@ -5,7 +5,10 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 object LeetCodeClient {
-    private const val URL = "https://leetcode.com/graphql"
+    private const val DOMAIN = "https://leetcode.com"
+    private const val GRAPHQL_URL = "$DOMAIN/graphql"
+    private const val LANG_KOTLIN = "Kotlin"
+
     private const val QUERY = """
         query GetDailyLeetCodeProblem {
             activeDailyCodingChallengeQuestion {
@@ -62,13 +65,51 @@ object LeetCodeClient {
                 }
             }
         }
+
+        val name: String by lazy {
+            "${data.activeDailyCodingChallengeQuestion.question.questionFrontendId}. ${data.activeDailyCodingChallengeQuestion.question.title}"
+        }
+
+        val url: String by lazy {
+            "$DOMAIN${data.activeDailyCodingChallengeQuestion.link}"
+        }
+
+        val sampleCodes: List<String> by lazy {
+            data.activeDailyCodingChallengeQuestion.question.codeSnippets
+                .find { it.lang == LANG_KOTLIN }
+                ?.code
+                ?.split("\n")
+                ?: throw Exception("No Kotlin code snippet found")
+        }
+
+        val methodParametersAndResultAsString: String by lazy {
+            sampleCodes[1]
+                .substringAfter('(')
+                .substringBefore('{')
+                .split(", ", "): ")
+                .let {
+                    val lastIndex = it.lastIndex
+                    it.mapIndexed { index, s ->
+                        if (index != lastIndex) {
+                            "val ${s.trim()}"
+                        } else {
+                            "val result: ${s.trim()}"
+                        }
+                    }
+                }.joinToString(", ")
+        }
+
+        val exampleTestcases: String by lazy {
+            data.activeDailyCodingChallengeQuestion.question.exampleTestcases
+                .replace("\n", " / ")
+        }
     }
 
     suspend fun getDailyLeetCodeProblem(): DailyLeetCodeProblem {
         val response =
             Fuel
                 .post(
-                    url = URL,
+                    url = GRAPHQL_URL,
                     headers = mapOf("Content-Type" to "application/json"),
                     body = Json.encodeToString(mapOf("query" to TRIM_INDENTED_QUERY)),
                 ).source
