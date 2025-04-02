1b78e40e49d1b940b2d89a4b1caaaf94a864548b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-04-03 01:19:00 +0900

    feat: Add LeetCodeProblem

---
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/LeetCodeProblem.kt
@@ -0,0 +1,9 @@
+package me.bossm0n5t3r.leetcode
+
+data class LeetCodeProblem(
+    val name: String,
+    val url: String,
+    val sampleCodes: List<String>,
+    val methodParametersAndResultAsString: String,
+    val exampleTestcases: String,
+)
