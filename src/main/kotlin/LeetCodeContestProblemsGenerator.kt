5888fb296c7f703794772966bcd5bf591afd1276
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-04-06 15:10:00 +0900

    feat: Add LeetCodeContestProblemsGenerator

---
+++ src/main/kotlin/LeetCodeContestProblemsGenerator.kt
@@ -0,0 +1,155 @@
+import LeetCodeHelper.toLowerCase
+import LeetCodeHelper.toPascalCase
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import java.nio.file.Paths
+import kotlin.io.path.exists
+
+object LeetCodeContestProblemsGenerator {
+    private val projectDirAbsolutePath = Paths.get("").toAbsolutePath().toString()
+    private val problemPath = Paths.get(projectDirAbsolutePath, "src/main/kotlin/me/bossm0n5t3r/leetcode")
+    private val testPath = Paths.get(projectDirAbsolutePath, "src/test/kotlin/me/bossm0n5t3r/leetcode")
+
+    private data class Problem(
+        val name: String,
+        val url: String,
+        val sampleCodes: List<String>,
+        val methodParametersAndResultAsString: String,
+        val exampleTestcases: String,
+        val filePath: String,
+    )
+
+    fun run(contestSlug: String) {
+        readProblemTitleSlugs(contestSlug).forEach { titleSlug ->
+            with(readProblem(titleSlug)) {
+                this.create()
+            }
+        }
+    }
+
+    private fun Problem.create() {
+        println()
+        println("Problem: $name")
+        println("URL: $url")
+        println()
+        createFiles(this)
+        createTest(this)
+        println("Done!")
+    }
+
+    private fun readProblemTitleSlugs(contestSlug: String): List<String> =
+        runBlocking {
+            LeetCodeClient.getLeetCodeContestProblemTitleSlugsByContestSlug(contestSlug)
+        }
+
+    private fun readProblem(titleSlug: String): Problem {
+        val problem = runBlocking { LeetCodeClient.getLeetCodeProblemByTitleSlug(titleSlug) }
+        return Problem(
+            name = problem.name,
+            url = problem.url,
+            sampleCodes = problem.sampleCodes,
+            methodParametersAndResultAsString = problem.methodParametersAndResultAsString,
+            exampleTestcases = problem.exampleTestcases,
+            filePath = problem.name.toLowerCase(),
+        )
+    }
+
+    private fun createFiles(problem: Problem) {
+        val (name, url, sampleCodes, _, _, filePath) = problem
+        val newProblemPath = Paths.get(problemPath.toString(), filePath)
+        try {
+            if (newProblemPath.exists()) {
+                println("Problem already exists!")
+                newProblemPath.toFile().deleteRecursively()
+                println("Previous problem deleted!")
+                println()
+            }
+
+            newProblemPath.toFile().mkdirs()
+            println("Created directory: ${newProblemPath.toAbsolutePath()}\n")
+
+            // Create README.md
+            File(newProblemPath.toString(), "README.md").writeText(
+                """
+                # $name
+                
+                - [$url]($url)
+                
+                """.trimIndent(),
+            )
+
+            // Create Problem
+            val pascalCaseProblemName = name.toPascalCase()
+            val sampleCodeString =
+                sampleCodes.joinToString("\n") {
+                    if (it.isNotEmpty()) {
+                        "    ${it.ifBlank { "${it}TODO()" }}"
+                    } else {
+                        ""
+                    }
+                }
+            File(newProblemPath.toString(), "$pascalCaseProblemName.kt").writeText(
+                "package me.bossm0n5t3r.leetcode.$filePath\n\n" +
+                    "class $pascalCaseProblemName {\n" +
+                    "${sampleCodeString}\n" +
+                    "}\n",
+            )
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+        }
+    }
+
+    private fun createTest(problem: Problem) {
+        val (name, _, _, methodParametersAndResultAsString, exampleTestcases, filePath) = problem
+        val newTestPath = Paths.get(testPath.toString(), filePath)
+        try {
+            if (newTestPath.exists()) {
+                println("Test already exists!")
+                newTestPath.toFile().deleteRecursively()
+                println("Previous test deleted!")
+                println()
+            }
+
+            newTestPath.toFile().mkdirs()
+            println("Created test directory: ${newTestPath.toAbsolutePath()}\n")
+
+            // Create Problem
+            val pascalCaseTestName = name.toPascalCase()
+            val pascalCaseTestClassName = "${pascalCaseTestName}Test"
+
+            File(newTestPath.toString(), "$pascalCaseTestClassName.kt").writeText(
+                """
+                package me.bossm0n5t3r.leetcode.$filePath
+
+                import org.junit.jupiter.api.Test
+                import kotlin.test.assertEquals
+
+                class $pascalCaseTestClassName {
+                    private val sut = $pascalCaseTestName.Solution()
+                    
+                    private data class TestData($methodParametersAndResultAsString)
+                    
+                    @Test
+                    fun test() {
+                        // Example Testcases
+                        // $exampleTestcases
+                        val testDataList = listOf(
+                            TestData(),
+                        )
+                        
+                        for (testData in testDataList) {
+                            assertEquals(
+                                testData.result,
+                                sut
+                            )
+                        }
+                    }
+                }
+
+                """.trimIndent(),
+            )
+        } catch (e: Exception) {
+            println("Error: ${e.message}")
+        }
+    }
+}
