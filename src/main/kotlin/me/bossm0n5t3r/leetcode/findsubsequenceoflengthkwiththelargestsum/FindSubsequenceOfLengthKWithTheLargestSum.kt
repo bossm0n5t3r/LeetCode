bf1c257bed2e6322803a2ef1a296fc8e69e91483
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-06-28 17:33:00 +0900

    2099. Find Subsequence of Length K With the Largest Sum

--- src/main/kotlin/me/bossm0n5t3r/leetcode/findsubsequenceoflengthkwiththelargestsum/FindSubsequenceOfLengthKWithTheLargestSum.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/findsubsequenceoflengthkwiththelargestsum/FindSubsequenceOfLengthKWithTheLargestSum.kt
@@ -5,13 +5,25 @@ class FindSubsequenceOfLengthKWithTheLargestSum {
         fun maxSubsequence(
             nums: IntArray,
             k: Int,
-        ): IntArray =
-            nums
-                .withIndex()
-                .sortedWith(compareByDescending<IndexedValue<Int>> { it.value }.thenBy { it.index })
-                .take(k)
-                .sortedBy { it.index }
-                .map { it.value }
-                .toIntArray()
+        ): IntArray {
+            val sortedDescendingNums = nums.sortedDescending()
+            val numberToCount = mutableMapOf<Int, Int>()
+            repeat(k) {
+                val key = sortedDescendingNums[it]
+                numberToCount[key] = numberToCount.getOrElse(key) { 0 } + 1
+            }
+
+            val result = IntArray(k)
+            var index = 0
+            for (num in nums) {
+                val value = numberToCount[num]
+                if (value != null && value > 0) {
+                    result[index++] = num
+                    numberToCount[num] = value - 1
+                }
+            }
+
+            return result
+        }
     }
 }
